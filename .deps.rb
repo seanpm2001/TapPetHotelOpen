ee58890b9bbc9cd9dee1f70931ba36357a3ed2ba
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-04-24 19:14:42 -0700

    .deps.rb [V1]
    Version 1 of the dependencies file fort his project.

---
+++ .deps.rb
@@ -0,0 +1,90 @@
+#!/usr/bin/ruby
+# Start of script
+# TapPetHotelOpen dependencies
+puts "TapPetHotelOpen dependencies"
+# Ruby
+# Windows
+# Ruby 2.5 (Windows XP)
+# Ruby 2.6 (Windows Vista)
+# Ruby 2.7 (Windows 7)
+# Ruby 3.1 (Windows 8)
+# Ruby 3.1 (Windows 8.1)
+# MacOS
+# Ruby 1.8 (Mac OS X 10.0 to Mac OS X 10.5)
+# Ruby 2.1 (Mac OS X 10.6 to OS X 10.10)
+# Ruby 2.6 (OS X 11.11 to MacOS 10.15)
+# Linux
+# Debian
+# Ruby 1.8 (Debian 7)
+# Ruby 2.0 (Debian 8)
+# Ruby 2.4 (Debian 9)
+# Ruby 2.9 (Debian 10)
+# Ruby 3.1 (Debian 11)
+# RHEL (Red Hat Enteprise Linux)
+# Ruby 2.4 (RHEL 5)
+# Ruby 2.7 (RHEL 6)
+# Ruby 3.0 (RHEL 7)
+# Ruby 3.1.2 (RHEL 8)
+# Ubuntu
+# Ruby 1.8 (Ubuntu 8.04 to 10.04)
+# Ruby 2.1 (Ubuntu 10.10 to 12.04)
+# Ruby 2.5 (Ubuntu 12.10 to 14.04)
+# Ruby 2.7 (Ubuntu 14.10 to 16.04)
+# Ruby 3.1.2 (Ubuntu 16.10 to 22.04)
+# Fedora
+# Ruby 1.8 (Fedora 7 to 15)
+# Ruby 2.3 (Fedora 16 to 23)
+# Ruby 2.8 (Fedora 24 to 32)
+# Ruby 3.1.2 (Fedora 33 to 37)
+# Arch
+# Ruby 1.8 (2010-2012 versions)
+# Ruby 2.0 (2012-2014 versions)
+# Ruby 2.4 (2014-2016 versions)
+# Ruby 2.7 (2016-2018 versions)
+# Ruby 3.0 (2018-2022 versions)
+puts "Ruby"
+puts "Windows"
+puts "Ruby 2.5 (Windows XP)"
+puts "Ruby 2.6 (Windows Vista)"
+puts "Ruby 2.7 (Windows 7)"
+puts "Ruby 3.1 (Windows 8)"
+puts "Ruby 3.1 (Windows 8.1)"
+puts "MacOS"
+puts "Ruby 1.8 (Mac OS X 10.0 to Mac OS X 10.5)"
+puts "Ruby 2.1 (Mac OS X 10.6 to OS X 10.10)"
+puts "Ruby 2.6 (OS X 11.11 to MacOS 10.15)"
+puts "Linux"
+puts "Debian"
+puts "Ruby 1.8 (Debian 7)"
+puts "Ruby 2.0 (Debian 8)"
+puts "Ruby 2.4 (Debian 9)"
+puts "Ruby 2.9 (Debian 10)"
+puts "Ruby 3.1 (Debian 11)"
+puts "RHEL (Red Hat Enteprise Linux)"
+puts "Ruby 2.4 (RHEL 5)"
+puts "Ruby 2.7 (RHEL 6)"
+puts "Ruby 3.0 (RHEL 7)"
+puts "Ruby 3.1.2 (RHEL 8)"
+puts "Ubuntu"
+puts "Ruby 1.8 (Ubuntu 8.04 to 10.04)"
+puts "Ruby 2.1 (Ubuntu 10.10 to 12.04)"
+puts "Ruby 2.5 (Ubuntu 12.10 to 14.04)"
+puts "Ruby 2.7 (Ubuntu 14.10 to 16.04)"
+puts "Ruby 3.1.2 (Ubuntu 16.10 to 22.04)"
+puts "Fedora"
+puts "Ruby 1.8 (Fedora 7 to 15)"
+puts "Ruby 2.3 (Fedora 16 to 23)"
+puts "Ruby 2.8 (Fedora 24 to 32)"
+puts "Ruby 3.1.2 (Fedora 33 to 37)"
+puts "Arch"
+puts "Ruby 1.8 (2010-2012 versions)"
+puts "Ruby 2.0 (2012-2014 versions)"
+puts "Ruby 2.4 (2014-2016 versions)"
+puts "Ruby 2.7 (2016-2018 versions)"
+puts "Ruby 3.0 (2018-2022 versions)"
+break
+# File info
+# File type dependency file (Ruby) (*.deps)
+# File version: 1 (2022, Sunday, April 24th at 7:14 pm PST)
+# Line count (including blank lines and compiler line): 91
+# End of script
